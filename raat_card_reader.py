35a2f1136a6ffc9d7e5fb56b0429566a3b14cf10
Cave Escape Pi <jamesfowkes@gmail.com>
2020-02-24 21:25:51 +0000

    Update to work with IR-based film reel sensing

--- raat_card_reader.py
+++ raat_card_reader.py
@@ -2,6 +2,7 @@ import threading
 import queue
 
 import logging
+import logging.handlers
 
 import time
 import requests
@@ -12,6 +13,16 @@ SCAN_TIME = 0.5
 
 def get_logger():
     return logging.getLogger(__name__)
+
+def strip_quotes(s):
+    if s[0] == "'":
+        s = s[1:]
+
+    if s[-1] == "'":
+        s = s[0:-1]
+
+    return s
+
 
 class RAATCardReader(threading.Thread):
 
@@ -25,44 +36,33 @@ class RAATCardReader(threading.Thread):
         self.runflag = True
         self.last_reply = ""
 
-    def read_and_forget(self):
-        self.reader.write("/device/01/?\r\n".encode())
+    def read(self):
+        self.reader.write("/param/01/?\r\n".encode())
         reply = self.reader.readline().decode("ascii").strip()
-
-        if reply != "NOCARD":
-            self.reader.write("/device/01/F\r\n".encode())
-            self.reader.readline()
-
         return reply
 
     def run(self):
 
         while self.runflag:
-            reply = self.read_and_forget()
+            reply = strip_quotes(self.read())
 
             if reply != self.last_reply:
                 self.last_reply = reply
                 uid = reply
-                get_logger().info("Reader got UID {}".format(uid))
-                if self.queue:
-                    get_logger().info("Putting on queue.")
-                    self.queue.put(uid)
-                if self.url:
-                    full_url = self.url.format(uid=uid)
-                    get_logger().info("Posting to URL {}".format(full_url))
-                    try:
-                    	requests.get(full_url, timeout=1)
-                    except requests.exceptions.ConnectionError:
-                        get_logger().info("Connection error when opening {}".format(full_url))
-                    except requests.exceptions.Timeout:
-                        get_logger().info("Timeout when opening {}".format(full_url))
-                if reply == "NOCARD":
-                    get_logger().info("Restarting Arduino")
-                    self.reader.close()
-                    self.reader = serial.Serial(self.device, 115200, timeout=1)
-                    time.sleep(1.5)
-                else:
-                    get_logger().info("Continuing RFID scan")
+                if len(uid):
+                    get_logger().info("Reader got UID {}".format(uid))
+                    if self.queue:
+                        get_logger().info("Putting on queue.")
+                        self.queue.put(uid)
+                    if self.url:
+                        full_url = self.url.format(uid=uid)
+                        get_logger().info("Posting to URL {}".format(full_url))
+                        try:
+                    	    requests.get(full_url, timeout=1)
+                        except requests.exceptions.ConnectionError:
+                            get_logger().info("Connection error when opening {}".format(full_url))
+                        except requests.exceptions.Timeout:
+                            get_logger().info("Timeout when opening {}".format(full_url))
 
             time.sleep(SCAN_TIME)
 
@@ -75,7 +75,11 @@ def setup_logging(handler):
 
 if __name__ == "__main__":
 
-    logging.basicConfig(level=logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging_handler = logging.handlers.RotatingFileHandler("cardreader.log", maxBytes=1024*1024, backupCount=3)
+    logging_handler.setFormatter(formatter)
+    get_logger().setLevel(logging.INFO)
+    get_logger().addHandler(logging_handler)
 
     reader_queue = queue.Queue()
     reader = RAATCardReader(queue=reader_queue)
